68364870902b0164ceb9e438ec9b73b100f9d8be
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-07-23 21:27:25 +0545

    login api completed

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -4,11 +4,12 @@ const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 const crypto = require("crypto");
 
+// Register user
 exports.registerUser = async (req, res) => {
   try {
     const { username, email,  password, role } = req.body;
 
-   
+    // Check for duplicate username, email, and phone
     const existingUsername = await User.findOne({ username });
     if (existingUsername) {
       return res.status(400).json({ error: "Username already exists. Please try another." });
@@ -18,14 +19,48 @@ exports.registerUser = async (req, res) => {
     if (existingEmail) {
       return res.status(400).json({ error: "Email is already in use. Please use a different email." });
     }
- 
+
+   
+
+    // Check if an avatar file was uploaded
     const avatar = req.file ? `/uploads/${req.file.filename}` : "";
 
-  
+    // Create and save new user, including avatar if provided
     const user = new User({ username, email, password, role, avatar });
     await user.save();
 
     res.status(201).json({ success: "User registered successfully!" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+// Login user
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if the email exists in the database
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ error: "Email does not exist" });
+    }
+
+    // Compare the provided password with the hashed password in the database
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ error: "Incorrect password" });
+    }
+
+    // Generate JWT Token
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.json({ token, success: "Login successful!" });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
