1760ea8c7fe9423f06e3d431fde7b935daa2e39e
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-07-24 20:25:05 +0545

    google map added in footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -64,7 +64,7 @@ const Footer = () => {
           </Link>
         </div>
 
-       
+        {/* Quick Links */}
         <div>
           <h2 className="text-lg font-semibold mb-4">Quick Links</h2>
           <ul className="space-y-2 text-sm text-[#8b8e99]">
@@ -91,7 +91,21 @@ const Footer = () => {
           </ul>
         </div>
 
-      
+        {/* Google Map */}
+        <div>
+          <h2 className="text-lg font-semibold mb-4">Google Maps</h2>
+          <iframe
+            title="Google Map"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3532.207617926562!2d85.28493277462759!3d27.70895527619113!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb198ed174aef5%3A0x37c163c2ea2e267b!2sKalanki!5e0!3m2!1sen!2snp!4v1688229345211!5m2!1sen!2snp"
+            width="100%"
+            height="150"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            className="rounded-lg"
+          ></iframe>
+        </div>
       </div>
 
     
