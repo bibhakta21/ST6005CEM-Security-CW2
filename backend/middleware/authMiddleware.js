d2163268fe3222eb181a8aba05119546f8584c5e
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-07-24 20:14:18 +0545

    auth middleware completed

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -8,7 +8,7 @@ exports.authMiddleware = async (req, res, next) => {
     return res.status(401).json({ error: "Access Denied, Token Missing" });
   }
 
-  const token = authHeader.split(" ")[1]; // Extract token after "Bearer"
+  const token = authHeader.split(" ")[1];
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -16,9 +16,21 @@ exports.authMiddleware = async (req, res, next) => {
 
     if (!user) return res.status(404).json({ error: "User not found" });
 
-    // If the password was changed after the token was issued, reject the token
     if (user.passwordChangedAt && decoded.iat * 1000 < user.passwordChangedAt.getTime()) {
       return res.status(401).json({ error: "Token expired due to password change. Please log in again." });
-    
-    }}
-  };
+    }
+
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Invalid Token" });
+  }
+};
+
+
+exports.adminMiddleware = (req, res, next) => {
+  if (!req.user || req.user.role !== "admin") {
+    return res.status(403).json({ error: "Access Forbidden, Admins Only" });
+  }
+  next();
+};
