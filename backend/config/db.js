a2d94d6f98dd199652df01214ac0172b56e8186d
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-07-27 22:29:58 +0545

    updated database name to nepalwears

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/db_ecommerce");
+    await mongoose.connect("mongodb://127.0.0.1:27017/db_nepalwears");
     console.log("MongoDB Connected Successfully!");
   } catch (error) {
     console.error("MongoDB Connection Failed:", error.message);
