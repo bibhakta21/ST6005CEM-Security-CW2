025bd0f169f726ba8ae9beccc3c6ce4b0f585a24
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-07-30 23:15:14 +0545

    aes util setup done

---
+++ backend/middleware/aesUtil.js
@@ -0,0 +1,24 @@
+// utils/aesUtil.js
+const crypto = require("crypto");
+
+const ENCRYPTION_KEY = process.env.AES_SECRET_KEY || "12345678901234567890123456789012"; // 32 bytes
+const IV_LENGTH = 16;
+
+function encrypt(text) {
+  const iv = crypto.randomBytes(IV_LENGTH);
+  const cipher = crypto.createCipheriv("aes-256-cbc", Buffer.from(ENCRYPTION_KEY), iv);
+  let encrypted = cipher.update(text, "utf8", "hex");
+  encrypted += cipher.final("hex");
+  return iv.toString("hex") + ":" + encrypted;
+}
+
+function decrypt(encryptedText) {
+  const [ivHex, encrypted] = encryptedText.split(":");
+  const iv = Buffer.from(ivHex, "hex");
+  const decipher = crypto.createDecipheriv("aes-256-cbc", Buffer.from(ENCRYPTION_KEY), iv);
+  let decrypted = decipher.update(encrypted, "hex", "utf8");
+  decrypted += decipher.final("utf8");
+  return decrypted;
+}
+
+module.exports = { encrypt, decrypt };
