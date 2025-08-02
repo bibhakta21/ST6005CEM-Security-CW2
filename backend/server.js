14a878aa952df94c2a1d5d435f30adb130f67634
bibhakta21 <bibhaktalamsal8@gmail.com>
2025-08-02 21:04:05 +0545

    added ssl certificates

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
+const fs = require("fs");
+const https = require("https");
 const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const sslOptions = {
+  key: fs.readFileSync("../ssl/key.pem"),
+  cert: fs.readFileSync("../ssl/cert.pem"),
+};
 
-module.exports = server; 
+https.createServer(sslOptions, app).listen(PORT, () => {
+  console.log(`HTTPS Server running on https://localhost:${PORT}`);
+});
